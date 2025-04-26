686d9a4dceac04b0e932123c0448ef9780c3adc7
marcely melo <marcelyjfmelo@gmail.com>
2025-04-26 18:37:45 -0300

    Aula 11

---
+++ src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <div className='navbar-content'>
+        <h2>Sasuke Uchiha</h2>
+        <ul>
+            <a href="#">Home</a>
+            <a href="#">Sobre mim</a>
+            <a href="#">Habilidades</a>
+            <a href="#">Projeto</a>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
